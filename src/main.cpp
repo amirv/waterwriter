9508fe370d565b4c10e41e8175c6a4bded32c706
Amir Vadai <amir@vadai.me>
2023-04-14 23:00:45 +0300

    Writing text with encoder
    Signed-off-by: Amir Vadai <amir@vadai.me>

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
 #include "font8x8.h"
+#include <ESP32Encoder.h>
+
+ESP32Encoder encoder;
 
 // Font was generated here: https://rop.nl/truetype2gfx/
 
-String text = "Hello World";
+#define STEPS_PER_CM 8 // 40 // 1 full cycle is 1cm
+#define PIXEL_CM 1       // Every pixel is 1cm len
+#define LETTER_PIXELS 8
+#define PIXEL_PAINT_MS 1000
+
+static String text = "Hello World";
+static int total_text_steps;
 
 struct fconv {
   int start_idx;
@@ -16,7 +25,7 @@ static struct fconv fconv[] = {
   { 0 },
 };
 
-static const uint8_t *get_glyph(char c)
+static const uint8_t *get_glyph(const char c)
 {
   struct fconv *f = fconv;
 
@@ -30,10 +39,12 @@ static const uint8_t *get_glyph(char c)
   return NULL;
 }
 
-void setup() {
-  Serial.begin(9600);
-  while (!Serial);  // wait for serial port to connect. Needed for native USB
-  
+static void setup_text()
+{
+  total_text_steps = text.length() * PIXEL_CM * STEPS_PER_CM * LETTER_PIXELS;
+  return;
+
+
   byte tempChrByte   = 0;
   char tempChr       = ' ';
 
@@ -62,5 +73,81 @@ void setup() {
   Serial.println("\n");
 }
 
-void loop() {
+static void setup_encoder()
+{
+	// Enable the weak pull down resistors
+
+	//ESP32Encoder::useInternalWeakPullResistors=DOWN;
+	// Enable the weak pull up resistors
+	ESP32Encoder::useInternalWeakPullResistors=UP;
+
+	// use pin 19 and 18 for the first encoder
+	encoder.attachHalfQuad(19, 18);
+		
+	// set starting count value after attaching
+	// encoder.setCount(37);
+
+	// clear the encoder's raw count and set the tracked count to zero
+	encoder.clearCount();
+	Serial.println("Encoder Start = " + String((int32_t)encoder.getCount()));
+}
+
+static void loop_encoder()
+{
+	// Loop and read the count
+	Serial.println("Encoder count = " + String((int32_t)encoder.getCount()));
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  while (!Serial);  // wait for serial port to connect. Needed for native USB
+
+  setup_text();
+  setup_encoder();  
+}
+
+void loop()
+{
+  static unsigned long stop_painting_ts;
+  static unsigned long next;
+  static int prev_text_letter = -1;
+  static int prev_letter_pixel = -1;
+  
+  int steps = encoder.getCount();
+  int text_step = steps % total_text_steps;
+  int text_letter = (text_step / (PIXEL_CM * STEPS_PER_CM)) / LETTER_PIXELS;
+  int letter_pixel = (text_step / (PIXEL_CM * STEPS_PER_CM)) % LETTER_PIXELS;
+
+  if (prev_text_letter == text_letter && prev_letter_pixel == letter_pixel) {
+    if (stop_painting_ts && millis() > stop_painting_ts) {
+      Serial.println("Stop painting");
+      stop_painting_ts = 0;
+    }
+
+    return;
+  }
+
+  //if (next && millis() < next)
+  //  return;
+
+  //next = millis() + 1000;
+
+  char letter = text.charAt(text_letter);
+
+  const uint8_t *glyph = get_glyph(letter);
+  if (!glyph)
+    glyph = get_glyph('X');
+
+  uint8_t pixels = pgm_read_byte_near(glyph + letter_pixel);
+
+  for (int i = 7; i >= 0; i--) {
+      Serial.print((pixels & 1<<i) ? '*' : ' ');
+  }
+
+  Serial.printf("  steps: %d | text_step: %d letter: %c pixel: %d.%#x - pixels: %#x\n", steps, text_step, letter, text_letter, letter_pixel, *glyph, pixels);
+
+  prev_text_letter = text_letter;
+  prev_letter_pixel = letter_pixel;
+  stop_painting_ts = millis() + PIXEL_PAINT_MS;
 }
